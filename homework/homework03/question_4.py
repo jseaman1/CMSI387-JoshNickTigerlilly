78c630ad0093419486cc4b800656aa08ba0c2fe3
Nick Morgan <nmorgan8@lion.lmu.edu>
2021-04-14 18:04:10 -0700

    added timer and tests

--- homework/homework03/question_4.py
+++ homework/homework03/question_4.py
@@ -1,17 +1,21 @@
 import mmap
+import time
 
 def check_for_x(filename):
+  t = time.time()
   with open(filename, mode="r", encoding="utf8") as file_obj:
     with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as mmap_obj:
       text = mmap_obj.read()
-      print(text)
       for byte in text:
         # lower case x = 120
         # upper case X = 88
         if byte == 120 or byte == 88:
-          print("success you found x!")
+          print("success! you found x in: {} seconds".format(time.time() - t))
           return
-      print("failure no x found!")
+      print("failure! you didn't find x in: {} seconds".format(time.time() - t))
 
-check_for_x("success_test.txt")
-check_for_x("failure_test.txt")
+check_for_x("q4_test/early_success.txt")
+check_for_x("q4_test/late_success.txt")
+check_for_x("q4_test/short_fail.txt")
+check_for_x("q4_test/long_fail.txt")
+
