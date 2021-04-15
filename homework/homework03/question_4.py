e7f95e9f06f5e93de15534b9a07f998c90c45fd7
Nick Morgan <45604201+nmorgan8@users.noreply.github.com>
2021-04-14 17:51:19 -0700

    finished question 4

---
+++ homework/homework03/question_4.py
@@ -0,0 +1,16 @@
+import mmap
+
+def check_for_x(filename):
+  with open(filename, mode="r", encoding="utf8") as file_obj:
+    with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as mmap_obj:
+      text = mmap_obj.read()
+      print(text)
+      for byte in text:
+        # lower case x = 120
+        # upper case X = 88
+        if byte == 120 or byte == 88:
+          print("success you found x!")
+          return
+      print("failure no x found!")
+
+check_for_x("test.txt")
